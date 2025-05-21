ebaa833363db2ca20c0f62c6915a9616bd57419d
Henrique Almeida <henriquealmpin@gmail.com>
2025-05-21 10:45:15 -0300

    some changes - index and tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', 'sans-serif'],
+        sans: ['Inter', 'ui-sans-serif', 'system-ui'],
       },
     },
   },
