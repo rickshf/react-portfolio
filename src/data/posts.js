4657c2630095c238d55e9f6e63d4a4e98f19f0b3
Henrique Almeida <henriquealmpin@gmail.com>
2025-05-21 14:42:41 -0300

    added posts.js

---
+++ src/data/posts.js
@@ -0,0 +1,11 @@
+// src/data/posts.js
+import primeiroPost from '../posts/primeiro-post.md?raw';
+
+export const posts = [
+  {
+    slug: 'primeiro-post',
+    title: 'Por que escolhi Tailwind para meu portfólio',
+    date: '2025-05-20',
+    content: primeiroPost,
+  },
+];
