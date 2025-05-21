caa2db73fde69ced17197af6497b9855227aa866
Henrique Almeida <henriquealmpin@gmail.com>
2025-05-21 10:03:38 -0300

    Homepage Added

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+export function Home() {
+  return (
+    <section>
+      <h2 className="text-3xl font-bold mb-4">Bem-vindo!</h2>
+      <p>
+        Sou um desenvolvedor apaixonado por criar experiências digitais
+        simples, elegantes e funcionais. Explore meus projetos e artigos.
+      </p>
+    </section>
+  );
+}
