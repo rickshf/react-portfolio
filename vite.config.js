d002aaa164fb83ce86ba3431d23f7f31004d7e76
Henrique Almeida <henriquealmpin@gmail.com>
2025-05-21 11:46:32 -0300

    some changes

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import md from 'vite-plugin-md';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    md({
-      markdownItOptions: {
-        html: true,
-      },
-    }),
-  ],
-});
+  plugins: [react()],
+})
