055850f86c1f638ced9182066debdce991f3bbda
Henrique Almeida <henriquealmpin@gmail.com>
2025-05-21 10:13:55 -0300

    Added contact

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+export function Contact() {
+  return (
+    <section>
+      <h2 className="text-3xl font-bold mb-4">Contato</h2>
+      <p>
+        Você pode me encontrar no <a href="https://linkedin.com" className="underline text-blue-600 dark:text-blue-400">LinkedIn</a>,
+        ou enviar um e-mail para <strong>seuemail@exemplo.com</strong>.
+      </p>
+    </section>
+  );
+}
