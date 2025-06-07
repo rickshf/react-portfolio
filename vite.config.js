8ae7e104ad2a9cebbd10750e985fb6eeafede8e0
Henrique Almeida <162052159+rickshf@users.noreply.github.com>
2025-06-07 15:35:38 -0300

    chore: remove vite-plugin-md

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import md from 'vite-plugin-md';
 
 export default defineConfig({
   // Use relative paths so the site works when opened from the file system
   base: './',
   plugins: [
     react(),
-    md({
-      markdownItOptions: {
-        html: true,
-      },
-    }),
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.md'],
